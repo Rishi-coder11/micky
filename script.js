b5dea3958bade90903aa708119e5922d9b7c860c
Rishi-coder11 <63031778+Rishi-coder11@users.noreply.github.com>
2020-09-08 18:49:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt("Who are you?")
+
+console.log(name);
+
+if(name === "Rishi") {
+    document.write('<input type="file" id="myFile" name="myFile"></input>')
+    document.write('<input type="file" id="myFile1" name="myFile1"></input>')
+    document.write('<input type="file" id="myFile2" name="myFile2"></input>')
+    document.write('<input type="file" id="myFile3" name="myFile3"></input>')
+    document.write('<input type="file" id="myFile4" name="myFile4"></input>')
+    document.write('<input type="file" id="myFile5" name="myFile5"></input>')
+    document.write('<input type="file" id="myFile6" name="myFile6"></input>')
+    document.write('<input type="file" id="myFile7" name="myFile7"></input>')
+    document.write('<input type="file" id="myFile8" name="myFile8"></input>')
+}    
